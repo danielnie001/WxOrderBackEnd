01369b8c574ae1201fe2db4fbf2ff90f5501facc
niehaisheng <haishengnie@163.com>
2021-10-26 20:08:11 +0800

    代码优化

--- odersystem/src/main/java/com/bobo/odersystem/OdersystemApplication.java
+++ odersystem/src/main/java/com/bobo/odersystem/OdersystemApplication.java
@@ -2,20 +2,11 @@ package com.bobo.odersystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class OdersystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OdersystemApplication.class, args);
     }
-
-    @RequestMapping("/")
-    public String sayHello(){
-        return "hello,world!";
-    }
-
 }
