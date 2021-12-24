85936971e796dd1600f55dbec1eb57d10daca94b
daniel <haishengnie@163.com>
2021-12-24 12:28:54 +0800

    文件位置调整

--- odersystem/src/main/java/com/bobo/ordersystem/common/config/SwaggerConfig.java
+++ odersystem/src/main/java/com/bobo/ordersystem/common/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.bobo.ordersystem.common;
+package com.bobo.ordersystem.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
