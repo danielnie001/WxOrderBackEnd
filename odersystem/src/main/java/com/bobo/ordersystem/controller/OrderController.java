cb0b6421d677f0e710511432af71ae79fe424e1a
daniel <haishengnie@163.com>
2021-12-24 12:24:59 +0800

    增加logback日志

--- odersystem/src/main/java/com/bobo/ordersystem/controller/OrderController.java
+++ odersystem/src/main/java/com/bobo/ordersystem/controller/OrderController.java
@@ -2,11 +2,12 @@ package com.bobo.ordersystem.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/orders")
@@ -14,10 +15,12 @@ import lombok.AllArgsConstructor;
 @Api(value = "订单系统")
 public class OrderController {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
     @ApiOperation(value = "订单系统")
     @GetMapping("/hello")
     public String hello(){
-        
+        log.info("*********************** 打印日志 **************************");
         return "Hello,SpringBootTest";
     }
 }
