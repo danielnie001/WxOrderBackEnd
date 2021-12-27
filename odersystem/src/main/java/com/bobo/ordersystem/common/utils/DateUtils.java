78a006435e0a9e7493f44b235f3b702f6d10e85b
daniel <haishengnie@163.com>
2021-12-27 15:39:39 +0800

    增加日期工具类

--- odersystem/src/main/java/com/bobo/ordersystem/common/utils/DateUtils.java
+++ odersystem/src/main/java/com/bobo/ordersystem/common/utils/DateUtils.java
@@ -14,11 +14,11 @@ import java.util.Date;
  */
 public class DateUtils {
 
-    public static final String DAFAULT_DATE_FORMAT = "yyyy-M-d";
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-M-d";
     public static final String DATE_FORMAT = "yyyy-MM-dd";
-    public static final String DAFAULT_DATETIME_FORMAT = "yyyy-M-d HH:mm:ss";
+    public static final String DEFAULT_DATETIME_FORMAT = "yyyy-M-d HH:mm:ss";
     public static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    public static final String DAFAULT_TIME_FORMAT = "HH:mm:ss";
+    public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
 
     /**
      * 默认构造方法
